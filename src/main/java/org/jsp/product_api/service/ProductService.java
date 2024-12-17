5148c348300fb9941a6e531a48744a97f2fc0c6c
dhanushtallam <dhanushtallam03@gmail.com>
2024-12-17 18:22:39 +0530

    delete and update

--- src/main/java/org/jsp/product_api/service/ProductService.java
+++ src/main/java/org/jsp/product_api/service/ProductService.java
@@ -36,7 +36,7 @@ public class ProductService {
 
 		return new ResponseEntity<Object>(map, HttpStatus.CREATED);
 	}
-	
+
 	public ResponseEntity<Object> fetchAllProducts() {
 		List<Product> list = repository.findAll();
 		if (list.isEmpty()) {
@@ -50,50 +50,26 @@ public class ProductService {
 			return new ResponseEntity<Object>(map, HttpStatus.OK);
 		}
 	}
+
 	public ResponseEntity<Object> fetchById(int id) {
 		Optional<Product> optional = repository.findById(id);
-		if(optional.isEmpty()) {
+		if (optional.isEmpty()) {
 			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("error", "No Products Found with Id: "+id);
+			map.put("error", "No Products Found with Id: " + id);
 			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
-		}else {
+		} else {
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("message", "Products Found");
 			map.put("data", optional.get());
 			return new ResponseEntity<Object>(map, HttpStatus.OK);
 		}
 	}
+
 	public ResponseEntity<Object> fetchByName(String name) {
 		List<Product> list = repository.findByName(name);
 		if (list.isEmpty()) {
 			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("error", "No Products Found with Name :"+name);
-			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
-		} else {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("message", "Products Found");
-			map.put("data", list);
-			return new ResponseEntity<Object>(map, HttpStatus.OK);
-		}
-	}
-	public ResponseEntity<Object> fetchByPriceGreater(double price) {
-		List<Product> list = repository.findByPriceGreaterThanEqual(price);
-		if (list.isEmpty()) {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("error", "No Products Found Price Greater Than: "+price);
-			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
-		} else {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("message", "Products Found");
-			map.put("data", list);
-			return new ResponseEntity<Object>(map, HttpStatus.OK);
-		}
-	}
-	public ResponseEntity<Object> fetchByStockBetween(int min, int max) {
-		List<Product> list = repository.findByStockBetween(min,max);
-		if (list.isEmpty()) {
-			Map<String, Object> map = new HashMap<String, Object>();
-			map.put("error", "No Products Found Stock Between: "+min+" and "+max);
+			map.put("error", "No Products Found with Name :" + name);
 			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
 		} else {
 			Map<String, Object> map = new HashMap<String, Object>();
@@ -103,4 +79,81 @@ public class ProductService {
 		}
 	}
 
+	public ResponseEntity<Object> fetchByPriceGreater(double price) {
+		List<Product> list = repository.findByPriceGreaterThanEqual(price);
+		if (list.isEmpty()) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("error", "No Products Found Price Greater Than: " + price);
+			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+		} else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("message", "Products Found");
+			map.put("data", list);
+			return new ResponseEntity<Object>(map, HttpStatus.OK);
+		}
+	}
+
+	public ResponseEntity<Object> fetchByStockBetween(int min, int max) {
+		List<Product> list = repository.findByStockBetween(min, max);
+		if (list.isEmpty()) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("error", "No Products Found Stock Between: " + min + " and " + max);
+			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+		} else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("message", "Products Found");
+			map.put("data", list);
+			return new ResponseEntity<Object>(map, HttpStatus.OK);
+		}
+	}
+
+	public ResponseEntity<Object> deleteById(int id) {
+		Optional<Product> optional = repository.findById(id);
+		if (optional.isEmpty()) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("error", "No Product Found with Id: " + id);
+			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+		} else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			repository.deleteById(id);
+			map.put("message", "Product Deleted Success");
+			return new ResponseEntity<Object>(map, HttpStatus.OK);
+		}
+	}
+
+	public ResponseEntity<Object> updateProduct(Product product) {
+		repository.save(product);
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("message", "Product Updated Success");
+		return new ResponseEntity<Object>(map, HttpStatus.OK);
+	}
+
+	public ResponseEntity<Object> updateProduct(int id, Product product) {
+		Optional<Product> optional = repository.findById(id);
+		if(optional.isEmpty()) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("error", "No Product Found with Id: "+id);
+			return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+		}else {
+			Map<String, Object> map = new HashMap<String, Object>();
+			
+			Product existingProduct = optional.get();
+			if(product.getName()!=null)
+				existingProduct.setName(product.getName());
+			if(product.getDescription()!=null)
+				existingProduct.setDescription(product.getDescription());
+			if(product.getPrice()!=0)
+				existingProduct.setPrice(product.getPrice());
+			if(product.getStock()!=0)
+				existingProduct.setStock(product.getStock());
+			
+			repository.save(existingProduct);
+			
+			
+			map.put("message", "Product Updated Success");
+			return new ResponseEntity<Object>(map, HttpStatus.OK);
+		}
+}
+	
 }
