faa05ec0153065bc928a6a21dccdffd87f34d95f
dhanushtallam <dhanushtallam03@gmail.com>
2024-12-17 15:22:25 +0530

    adding the products

---
+++ src/main/java/org/jsp/product_api/dto/Product.java
@@ -0,0 +1,49 @@
+package org.jsp.product_api.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String description;
+	private String stock;
+	private double price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStock() {
+		return stock;
+	}
+	public void setStock(String stock) {
+		this.stock = stock;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
---
+++ src/main/java/org/jsp/product_api/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.jsp.product_api.repository;
+
+import org.jsp.product_api.dto.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
+
+}
